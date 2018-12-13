37ed243bff2f9accd7b0b37e2494bd8313bdec0d
James Hotblack <james.c.hotblack@gmail.com>
2018-12-13 18:31:39 +0000

    POST/redirect/GET pattern

--- app.rb
+++ app.rb
@@ -1,17 +1,25 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+    enable :sessions
+
     get '/' do
-        "Welcome to Battle Royale!"
         erb(:index)
     end
 
       
     post '/names' do
-        @player_1_name = params[:player_1_name]
-        @player_2_name = params[:player_2_name]
-        erb :play
-      end
+        session[:player_1_name] = params[:player_1_name]
+        session[:player_2_name] = params[:player_2_name]
+        redirect '/play'
+    end
+
+    get '/play' do
+        @player_1_name = session[:player_1_name]
+        @player_2_name = session[:player_2_name]
+        erb(:play)
+    end
 
     run! if app_file == $0
 end
