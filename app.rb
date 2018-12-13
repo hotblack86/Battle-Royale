e52c4264340b2c93be607c30f723976e68c6063f
James Hotblack <james.c.hotblack@gmail.com>
2018-12-13 15:05:48 +0000

    Getting started with Battle

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
-require 'sinatra'
+require 'sinatra/base'
 
 class Battle < Sinatra::Base
     get '/' do
         "Welcome to Battle Royale!"
     end
 
-    run! if app_file == $0
+    #run! if app_file == $0
 end
